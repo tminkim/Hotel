49cc350d7117d5e8198c25bead4e443e9b32cf56
tminkim <ktmtem@naver.com>
2024-08-21 15:05:50 +0000

    Initial commit

--- src/components/Manual.js
+++ src/components/Manual.js
@@ -1,44 +1,44 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import PDFViewer from './PDFViewer';
 import pdfStructure from '../assets/pdfStructure.json';
 
 const Manual = () => {
-  const [selectedFile, setSelectedFile] = useState(null);
+    const [selectedFile, setSelectedFile] = useState(null);
 
-  return (
-    <div className="bg-white shadow-md rounded-lg overflow-hidden">
-      <div className="p-4">
-        <h3 className="text-lg font-semibold">매뉴얼</h3>
-        <div className="flex">
-          <div className="w-1/3">
-            {pdfStructure.folders.map(folder => (
-              <div key={folder.folderName} className="mb-2">
-                <h4 className="text-md font-semibold">{folder.folderName}</h4>
-                <ul>
-                  {folder.files.map(file => (
-                    <li
-                      key={file}
-                      className="cursor-pointer text-blue-500 hover:underline"
-                      onClick={() => setSelectedFile(require(`../assets/pdf/${folder.folderName}/${file}`))}
-                    >
-                      {file}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ))}
-          </div>
-          <div className="w-2/3">
-            {selectedFile ? (
-              <PDFViewer file={selectedFile} />
-            ) : (
-              <p>PDF 파일을 선택하세요.</p>
-            )}
-          </div>
+    return (
+        <div className="bg-white shadow-md rounded-lg overflow-hidden">
+            <div className="p-4">
+                <h3 className="text-lg font-semibold">매뉴얼</h3>
+                <div className="flex">
+                    <div className="w-1/3">
+                        {pdfStructure.folders.map(folder => (
+                            <div key={folder.folderName} className="mb-2">
+                                <h4 className="text-md font-semibold">{folder.folderName}</h4>
+                                <ul>
+                                    {folder.files.map(file => (
+                                        <li
+                                            key={file}
+                                            className="cursor-pointer text-blue-500 hover:underline"
+                                            onClick={() => setSelectedFile(require(`../assets/pdf/${folder.folderName}/${file}`))}
+                                        >
+                                            {file}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="w-2/3">
+                        {selectedFile ? (
+                            <PDFViewer file={selectedFile} />
+                        ) : (
+                            <p>PDF 파일을 선택하세요.</p>
+                        )}
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Manual;
--- src/components/PDFViewer.js
+++ src/components/PDFViewer.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { Worker, Viewer } from '@react-pdf-viewer/core';
-import { pdfjs } from 'react-pdf';
 import '@react-pdf-viewer/core/lib/styles/index.css';
-import workerSrc from 'pdfjs-dist/build/pdf.worker.min.js';
-
-pdfjs.GlobalWorkerOptions.workerSrc = workerSrc;
 
 const PDFViewer = ({ file }) => {
     return (
         <div style={{ height: '750px' }}>
-            <Worker workerUrl={workerSrc}>
+            <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
                 <Viewer fileUrl={file} />
             </Worker>
         </div>
